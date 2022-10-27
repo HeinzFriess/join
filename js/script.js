e830246ce0e5309b2919f7ddf64548c41a70fb6a
Dennis Ammen <dennis.ammen@outlook.com>
2022-10-27 21:53:55 +0200

    Added includeHTML function

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
 'use strict';
 
+
+window.onload = () => {
+    includeHTML();
+}
+
+
+async function includeHTML() {
+    const includeElements = document.querySelectorAll('[template-html]');
+    for (let i = 0; i < includeElements.length; i++) {
+        const element = includeElements[i];
+        const file = element.getAttribute("template-html");
+        const resp = await fetch(file);
+        if (resp.ok) {
+            element.innerHTML = await resp.text();
+        } else {
+            element.innerHTML = 'Page not found';
+        }
+    }
+}
