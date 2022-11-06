fa8f32fb9221f673658042c0083c5c64e6115e6d
Dennis Ammen <dennis.ammen@outlook.com>
2022-11-06 14:36:11 +0100

    Added function for toggeling logout modal

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 'use strict';
 
 
-window.onload = () => {
-    includeHTML();
+async function init() {
+    await includeHTML();
+    logoutModalEventListener();
 }
 
-
+/**
+ * Inserts the HTML from the template files into the referenced file.
+ */
 async function includeHTML() {
     const includeElements = document.querySelectorAll('[template-html]');
     for (let i = 0; i < includeElements.length; i++) {
@@ -19,3 +22,16 @@ async function includeHTML() {
         }
     }
 }
+
+
+/**
+ * Adds an event listener to the profile picture to toggle the logout modal.
+ */
+function logoutModalEventListener() {
+    document.getElementById('profile-picture').addEventListener('click', () => {
+        document.getElementById('logout-modal').classList.toggle('d-none');
+    });
+}
+
+
+init();
