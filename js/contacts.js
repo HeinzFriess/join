a3661a7fc2161a09793b94c8286c5731d454d419
Dennis Ammen <dennis.ammen@outlook.com>
2022-11-07 23:43:59 +0100

    Added functions to show and render contacts

--- js/contacts.js
+++ js/contacts.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const contactsContainer = document.getElementById('contacts-container');
 const modalBackground = document.getElementById('modal-background');
 const modalContent = document.getElementById('modal-content');
 const closeModalBtn = document.getElementById('close-modal');
@@ -9,16 +10,65 @@ const modalLabel = document.getElementById('modal-label');
 const cancelContact = document.getElementById('modal-cancel');
 const createEditContact = document.getElementById('modal-confirm');
 
+let contacts = [];
+let lastnameCharacters = new Set(['A', 'B', 'D']);
 
-modalBackground.addEventListener('click', hideModal);
-closeModalBtn.addEventListener('click', hideModal);
-addContact.addEventListener('click', () => showModal('add'));
-editContact.addEventListener('click', () => showModal('edit'));
-cancelContact.addEventListener('click', hideModal);
+
+async function init() {
+    addAllEventListeners();
+    contacts = await loadContacts();
+    renderContacts();
+}
+
+
+function addAllEventListeners() {
+    modalBackground.addEventListener('click', hideModal);
+    closeModalBtn.addEventListener('click', hideModal);
+    addContact.addEventListener('click', () => showModal('add'));
+    editContact.addEventListener('click', () => showModal('edit'));
+    cancelContact.addEventListener('click', hideModal);
+}
+
+
+async function loadContacts() {
+    return await fetch('/contacts.json').then(resp => resp.json());
+}
+
+function renderContacts() {
+    contactsContainer.innerHTML = '';
+
+    contacts.forEach((contact, index) => {
+        contactsContainer.innerHTML += contactCardTemp(contact, index);
+    })
+}
+
+function renderSeperators() {
+    contactsContainer.innerHTML = '';
+
+    lastnameCharacters.forEach(character => {
+        contactsContainer.innerHTML += contactSeparatorTemp(character);
+    })
+}
+
+function showContact(index) {
+    const nameEl = document.getElementById('contact-detail-name');
+    const mailEl = document.getElementById('contact-detail-mail');
+    const phoneEl = document.getElementById('contact-detail-phone');
+    const contactColor = document.getElementById('contact-color');
+    let contact = contacts[index];
+
+    nameEl.innerHTML = `${contact.firstname} ${contact.lastname}`;
+    mailEl.innerHTML = contact.email;
+    mailEl.href = `mailto:${contact.email}`;
+    phoneEl.innerHTML = contact.phone;
+    phoneEl.href = `tel:${contact.phone}`;
+    contactColor.style = `background: hsl(${contact.color}, 100%, 40%)`;
+}
 
 
 /**
  * Shows the modal for the add contact/edit contact form.
+ * @param {String} type 
  */
 function showModal(type) {
     modalBackground.classList.remove('d-none');
@@ -38,3 +88,32 @@ function hideModal() {
     modalContent.classList.add('d-none');
     modalContent.classList.remove('modal-slide-in');
 }
+
+
+// -------------------
+// Templates
+// -------------------
+
+function contactCardTemp(contact, index) {
+    return `
+        <div class="contact" onclick="showContact(${index})">
+            <div style="background: hsl(${contact.color}, 100%, 40%);">
+                <span>FL</span>
+            </div>
+            <div>
+                <span>${contact.lastname}, ${contact.firstname}</span>
+                <span>${contact.email}</span>
+            </div>
+        </div>`;
+}
+
+
+function contactSeparatorTemp(letter) {
+    return `
+        <div class="contact-seperator">
+            <span>${letter}</span>
+        </div>`;
+}
+
+
+init();
