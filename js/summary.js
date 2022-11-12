36db802fae0537eac8121db564cdac95416a101d
Heinz Friess <110535964+HeinzFriess@users.noreply.github.com>
2022-11-13 00:55:38 +0100

    more functions

--- js/summary.js
+++ js/summary.js
@@ -1,15 +1,18 @@
 
 const urlParameter = new URLSearchParams(window.location.search);
 userID = urlParameter.get('userID');
+const heuteInSec = Date.now();
+const secTillUrgent = 604800000; // 7Days
 
 async function init() {
     await downloadFromServer();
     await loadTasks();
     await loadContacts();
-    renderGreeting();
+    renderHeadline();
+    renderUrgent()
 }
 
-function renderGreeting() {
+function renderHeadline() {
     let element = document.getElementById('headline');
     const contact = contacts.find(c => c.id == userID);
     if (userID > 0) {
@@ -18,6 +21,55 @@ function renderGreeting() {
     }
     else element.innerHTML = `<p style="font-weight: bold;">Good morning</p>`;
 
+}
+
+function renderUrgent() {
+    let content1 = document.getElementById('tasksUrgent');
+    let content2 = document.getElementById('deadlineDate');
+    const taskUrgentAmount = calcTasksUrgent().length;
+    let deadline = '';
+    if (taskUrgentAmount < 1) deadline = 'No';
+    else deadline = getMostUrgendTask().dueDate;
+
+    content1.innerHTML = `${taskUrgentAmount}`;
+    content2.innerHTML = `${deadline}`;
+}
+
+function calcTasksUrgent() {
+    let tasksExcludeDone = tasks.filter(t => t.status != 'Done');
+    let urgentTasks = [];
+    tasksExcludeDone.forEach(task => {
+        let timespan = (getSecFromDate(task.dueDate) - heuteInSec);
+        if ( timespan < secTillUrgent) urgentTasks.push(task.id);
+    });
+    return urgentTasks;
+}
+
+function getMostUrgendTask(){
+    let time = secTillUrgent;
+    calcTasksUrgent().forEach(taskID => {
+        task = tasks.find(t => t.id == taskID);
+        timeTemp = (getSecFromDate(task.dueDate) - heuteInSec);
+        if (timeTemp < time) {
+            time = timeTemp;
+            getMostUrgendTask = task;
+        }
+    })
+    return task;
+}
+
+function getSecFromDate(dateString) { //dateString DD.MM.YYY
+    let myDate = dateString.split(".");
+    let timeStamp = new Date(myDate[2], myDate[1]-1, myDate[0]).getTime();
+    return timeStamp;
+}
+
+function countTasks(status) {
+    let value = 0;
+    tasks.forEach(element => {
+        if (element.status == status) value++;
+    });
+    return value;
 }
 
 init();
