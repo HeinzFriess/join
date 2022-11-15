7f58cc265caab938e144d125d0983f5d40725e21
Dennis Ammen <dennis.ammen@outlook.com>
2022-11-16 00:00:49 +0100

    Added addTask function

--- js/task.js
+++ js/task.js
@@ -3,10 +3,11 @@
 const assigneeMenu = document.getElementById('assignee');
 const assigneeBackground = document.getElementById('assignee-background');
 const assigneeContainer = document.getElementById('assignee-container');
+const addTaskBtn = document.getElementById('add-task');
 
 
-// assigneeBackground.addEventListener('click', hideDropdown);
 assigneeMenu.addEventListener('click', toggleDropdown);
+addTaskBtn.addEventListener('click', addTask);
 
 
 /**
@@ -16,3 +17,25 @@ function toggleDropdown() {
     assigneeBackground.classList.toggle('d-none');
     assigneeContainer.classList.toggle('d-none');
 }
+
+
+function addTask() {
+    tasks.push({
+        "id" : 1,
+        "maintask" : true,
+        "status" : "To do",
+        "category" : "Design",
+        "title" : "Webdesite redesign",
+        "description" : "Modify the content of the main website...",
+        "priority" : "Low",
+        "dueDate" : "01.01.2023",
+        "subtasks" : [],
+        "assigned" : [1,2,3]
+    });
+
+    storeTasks();
+}
+
+
+loadTasks()
+
